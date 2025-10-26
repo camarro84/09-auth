6f9dc52cd51f5d53dc6e4cd64b8a66e67db9b99b
camarro84 <camarro84@gmail.com>
2025-10-26 18:29:58 +0100

    update files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,27 @@
 import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
 import './globals.css'
-import TanStackProvider from '@/components/TanStackProvider/TanStackProvider'
-import AuthProvider from '@/components/AuthProvider/AuthProvider'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
-
-const roboto = Roboto({
-  weight: ['400', '500', '700'],
-  subsets: ['latin'],
-  variable: '--font-roboto',
-})
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider'
+import AuthProvider from '@/components/AuthProvider/AuthProvider'
 
 export const metadata: Metadata = {
   title: 'NoteHub',
-  description: 'NoteHub Auth HW-09',
+  description: 'NoteHub application',
 }
 
 export default function RootLayout({
   children,
-  modal,
 }: {
   children: React.ReactNode
-  modal: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={roboto.variable} suppressHydrationWarning>
+      <body>
         <TanStackProvider>
           <AuthProvider>
             <Header />
             {children}
-            {modal}
             <Footer />
           </AuthProvider>
         </TanStackProvider>
