85d012ed030ec7db416b87d106b3d96711252763
camarro84 <camarro84@gmail.com>
2025-10-26 19:19:56 +0100

    update files for new structure

--- app/(private routes)/layout.tsx
+++ app/(private routes)/layout.tsx
@@ -1,16 +1,7 @@
-import React from 'react'
-
 export default function PrivateLayout({
   children,
-  modal,
 }: {
   children: React.ReactNode
-  modal: React.ReactNode
 }) {
-  return (
-    <>
-      {children}
-      {modal}
-    </>
-  )
+  return <>{children}</>
 }
